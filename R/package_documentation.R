1ed37d85b9bd83a359a62e743afc0c228e1b667b
Simon Andrews <simon.andrews@babraham.ac.uk>
2020-12-10 15:29:34 +0000

    Added package level documentation

---
+++ R/package_documentation.R
@@ -0,0 +1,15 @@
+#' sangr: A package for simulating sanger sequencing chromatograms
+#'
+#' The sangr package provides three main functions which you might
+#' want to use.
+#'
+#' @section sangr functions:
+#' simulate_sanger_data
+#'
+#' draw_chromatogram
+#'
+#' merge_sanger_data
+#'
+#' @docType package
+#' @name sangr
+NULL
