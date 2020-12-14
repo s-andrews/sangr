331fbc4367b7a030ef593a0cd9c94415e3abbe21
Simon Andrews <simon.andrews@babraham.ac.uk>
2020-12-14 15:37:23 +0000

    Allow custom colours in draw_chromatogram

--- R/draw_chromatogram.R
+++ R/draw_chromatogram.R
@@ -2,6 +2,7 @@
 #'
 #' @param data The per base density data from simulate_sanger_data
 #' @param sequence A string containing the expected sequence
+#' @param palette The colours used for the ACGT traces. Can be "ABI" or "Geneious"
 #'
 #' @return A ggplot object containing the chromatogram
 #' @export
@@ -12,12 +13,18 @@
 #'
 #' @examples
 #' draw_chromatogram(simulate_sanger_data("GAATTC"), "GAATTC")
-draw_chromatogram <- function(data, sequence) {
+draw_chromatogram <- function(data, sequence, palette="ABI") {
 
   # This avoids the spurious warnings from check()
   NULL -> POS -> base
 
   strsplit(sequence,"")[[1]] -> labels
+
+  palette_colours <- c("green2","blue2","black","red2")
+
+  if(palette == "Geneious") {
+    palette_colours <- c("red2","blue2","green2","darkgoldenrod2")
+  }
 
   data %>%
     tidyr::pivot_longer(
@@ -28,7 +35,7 @@ draw_chromatogram <- function(data, sequence) {
       ggplot2::ggplot(ggplot2::aes(x=POS,y=density, colour=base)) +
       ggplot2::geom_line(size=1.2, show.legend = FALSE) +
       ggplot2::scale_x_continuous(breaks = seq(from=40,by=20,length.out=length(labels)), labels = labels) +
-      ggplot2::scale_colour_manual(values = c("green2","blue2","black","red2")) +
+      ggplot2::scale_colour_manual(values = palette_colours) +
       ggplot2::theme(axis.title = ggplot2::element_blank(),
             axis.text.y = ggplot2::element_blank(),
             axis.ticks.y = ggplot2::element_blank()) %>%
